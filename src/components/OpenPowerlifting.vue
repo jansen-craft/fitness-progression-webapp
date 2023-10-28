bee07729a200c0eaab3ed96c0f0310830a97d6b3
j <jc103820@ohio.edu>
2023-10-28 12:03:21 -0400

    Add initial routing

---
+++ src/components/OpenPowerlifting.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <p>open powerlifting page</p>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,28 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+import HelloWorldVue from './components/HelloWorld.vue';
+import Home from './components/Home.vue';
+import Personal from './components/Personal.vue';
+import OpenPowerlifting from './components/OpenPowerlifting.vue';
+
+import AppVue from './App.vue';
+
+const routes = [
+  { path: '/hello', component: HelloWorldVue },
+  { path: '/', component: Home },
+  { path: '/open-powerlifting', component: OpenPowerlifting },
+  { path: '/personal', component: Personal },
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(AppVue)
+
+app.use(router)
+
+app.mount('#app')
